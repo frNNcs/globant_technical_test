e86ebac41c0520ea29445fc38abded5f64261c9b
francisco <frnncs@gmail.com>
2024-04-03 20:34:20 -0300

    rename test_allBerryStats to test

--- tests/test_berry_stats.py
+++ tests/test_berry_stats.py
@@ -1,6 +1,7 @@
 import pytest
 from httpx import AsyncClient
 
+from api.models.berry import BerryStats
 from main import app
 
 
@@ -9,5 +10,5 @@ async def test_allBerryStats_ok():
     async with AsyncClient(app=app, base_url='http://test') as client:
         response = await client.get('/allBerryStats')
 
-    assert response.status_code == 200
-    assert len(response.json()['berries_names']) == 64
+        assert response.status_code == 200
+        assert BerryStats(**response.json()).dict() == response.json()
