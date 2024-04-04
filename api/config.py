2bcb15bbdc86f920bcfbecbde42e806830ebaecd
francisco <frnncs@gmail.com>
2024-04-04 19:09:49 -0300

    add redis

---
+++ api/config.py
@@ -0,0 +1,16 @@
+import os
+
+import redis.asyncio as redis
+from dotenv import load_dotenv
+from redis.asyncio.client import Redis
+
+load_dotenv()
+
+REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379")
+CACHE_TIME = int(os.environ.get("CACHE_TIME", 60 * 60 * 10))
+
+redis_client : Redis = redis.Redis(
+    host='localhost',
+    port=6379,
+    decode_responses=True
+)
