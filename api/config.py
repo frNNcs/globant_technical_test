d27e149f154963770864603dec05ff8ebdfbdddf
francisco <frnncs@gmail.com>
2024-04-04 19:25:00 -0300

    change redis host

--- api/config.py
+++ api/config.py
@@ -10,7 +10,7 @@ REDIS_URL = os.environ.get("REDIS_URL", "redis://redis:6379")
 CACHE_TIME = int(os.environ.get("CACHE_TIME", 60 * 60 * 10))
 
 redis_client : Redis = redis.Redis(
-    host='localhost',
+    host='redis',
     port=6379,
     decode_responses=True
 )
